84dc2e49793fedc336e1631e653d557489c0ebe0
liqi <1832271620@qq.com>
2023-12-27 23:35:08 +0800

    feat(os-05): add comment

--- code/os/05-traps/trap.c
+++ code/os/05-traps/trap.c
@@ -7,13 +7,14 @@ void trap_init()
 	/*
 	 * set the trap-vector base-address for machine-mode
 	 */
+	// 将trap_vector函数的地址写入到mtvec寄存器中
 	w_mtvec((reg_t)trap_vector);
 }
 
 reg_t trap_handler(reg_t epc, reg_t cause)
 {
-	reg_t return_pc = epc;
-	reg_t cause_code = cause & 0xfff;
+	reg_t return_pc = epc;									// 保存返回地址
+	reg_t cause_code = cause & 0xfff;						// 保存中断原因
 	
 	if (cause & 0x80000000) {
 		/* Asynchronous trap - interrupt */
@@ -35,7 +36,7 @@ reg_t trap_handler(reg_t epc, reg_t cause)
 		/* Synchronous trap - exception */
 		printf("Sync exceptions!, code = %d\n", cause_code);
 		panic("OOPS! What can I do!");
-		//return_pc += 4;
+		// return_pc += 4;											// 返回下一条指令
 	}
 
 	return return_pc;
